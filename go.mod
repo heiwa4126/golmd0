8c0170b284511730275ea436214da946fb266507
heiwa4126 <heiwa4126@gmail.com>
2019-09-17 09:58:12 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/heiwa4126/golmd1
+
+go 1.13
+
+require github.com/aws/aws-lambda-go v1.13.2
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+// See:
+// <https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/go-programming-model-handler-types.html>
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
